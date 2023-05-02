696776ce0ce5168991b352449bb37851fe26bcca
Lee Simpson <harveysimpsondata@gmail.com>
2023-05-02 11:36:03 -0400

    05-02-23

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -3,9 +3,7 @@
 
 import os
 import argparse
-
 from time import time
-
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -13,15 +11,16 @@ from sqlalchemy import create_engine
 def main(params):
     user = params.user
     password = params.password
-    host = params.host 
-    port = params.port 
+    host = params.host
+    port = params.port
     db = params.db
     table_name = params.table_name
     url = params.url
-    
+
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
+
         csv_name = 'output.csv.gz'
     else:
         csv_name = 'output.csv'
@@ -41,12 +40,11 @@ def main(params):
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
-    while True: 
+    while True:
 
         try:
             t_start = time()
-            
+
             df = next(df_iter)
 
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
@@ -61,6 +59,7 @@ def main(params):
         except StopIteration:
             print("Finished ingesting data into the postgres database")
             break
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -9,3 +9,6 @@ day = sys.argv[1]
 # some fancy stuff with pandas
 
 print(f'job finished successfully for day = {day}')
+
+
+
